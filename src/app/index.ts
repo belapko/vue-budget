69417371c8eb61d95e1d30b339b7e5d6c180f416
belapko <belapko23@gmail.com>
2023-10-11 21:51:26 +0300

    initial setup

---
+++ src/app/index.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import { pinia, router } from './providers'
+
+export const app = createApp(App).use(pinia).use(router)
---
+++ src/pages/main/ui/MainPage.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div><h1>Main page</h1></div>
+</template>
